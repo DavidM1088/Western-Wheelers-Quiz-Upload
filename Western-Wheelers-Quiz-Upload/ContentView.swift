16d79aa4f0def3d5823723f5595738b34d1fbd0c
David Murphy <davidp.murphy@sbcglobal.net>
2020-07-30 18:44:34 -0700

    Initial Commit

---
+++ Western-Wheelers-Quiz-Upload/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Western-Wheelers-Quiz-Upload
+//
+//  Created by David Murphy on 7/30/20.
+//  Copyright © 2020 David Murphy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Western-Wheelers-Quiz-UploadTests/Western_Wheelers_Quiz_UploadTests.swift
@@ -0,0 +1,34 @@
+//
+//  Western_Wheelers_Quiz_UploadTests.swift
+//  Western-Wheelers-Quiz-UploadTests
+//
+//  Created by David Murphy on 7/30/20.
+//  Copyright © 2020 David Murphy. All rights reserved.
+//
+
+import XCTest
+@testable import Western_Wheelers_Quiz_Upload
+
+class Western_Wheelers_Quiz_UploadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
